6104ecb4bcab5576e9f124a47bfad11cb58113ba
DeltaTempest <raihan5.herwin@gmail.com>
2021-10-31 17:16:52 +0700

    Menyelesaikan soal no 1 pada Soal-1.py

---
+++ Soal-1.py
@@ -0,0 +1,7 @@
+#Nama: Raihan Herwin Utama
+#Kelas: AI MentorShip Basic Pyhton
+
+nama = input("Masukan nama Anda: ")
+umur = input("Masukan umur Anda: ")
+tinggi = input("Masukan tinggi badan anda: ")
+print("Nama saya {}, umur saya {} tahun dan tinggi saya {} cm.".format(nama, umur, tinggi))
