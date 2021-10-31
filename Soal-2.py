595beb9c787632b4e143c21f4778eaaa708c9306
DeltaTempest <raihan5.herwin@gmail.com>
2021-10-31 17:27:51 +0700

    Menyelesaikan soal no 2 pada Soal-2.py

---
+++ Soal-2.py
@@ -0,0 +1,6 @@
+#Nama: Raihan Herwin Utama
+r = input("Silahkan masukan jari jarinya: ")
+r = float(r)
+v = 22/7
+luasLingkaran = float(v*r*r)
+print("Luas lingkaran dengan jari-jari {} cm adalah {} cm\u00b2.".format(r, luasLingkaran))
